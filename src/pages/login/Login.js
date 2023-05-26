8742c06f8ca75846df5f5bf0a880ff472cdebef4
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-26 21:19:53 +0200

    feat: using login hook

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useLogin } from '../../hooks/useLogin';
 
 // styles
 import styles from './Login.module.css';
@@ -6,10 +7,11 @@ import styles from './Login.module.css';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { login, error, isPending } = useLogin();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password);
+    login(email, password);
   };
 
   return (
@@ -34,7 +36,16 @@ const Login = () => {
           value={password}
         />
       </label>
-      <button className='btn'>Login</button>
+      {!isPending && <button className='btn'>Login</button>}
+      {isPending && (
+        <button
+          className='btn'
+          disabled
+        >
+          loading
+        </button>
+      )}
+      {error && <p>{error}</p>}
     </form>
   );
 };

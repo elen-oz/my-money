49563be9697890c7c0f80ffb8ef85ba43d43b958
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-29 19:34:20 +0200

    feat: add list of transactions component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,24 @@
-import TransactionForm from './TransactionForm';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useCollection } from '../../hooks/useCollection';
 
 // styles
 import styles from './Home.module.css';
-import { useAuthContext } from '../../hooks/useAuthContext';
+
+// components
+import TransactionForm from './TransactionForm';
+import TransactionList from '../home/TransactionList';
 
 const Home = () => {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection('transactions');
 
   return (
     <div className={styles.container}>
-      <div className={styles.content}>transaction list</div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
+
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>

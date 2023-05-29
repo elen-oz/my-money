09205b49467475be1d211a34ca87a56260fa3fe1
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-29 20:17:33 +0200

    feat: add delete function

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,7 +1,12 @@
+import { useFirestore } from '../../hooks/useFirestore';
+
 // styles
 import styles from './Home.module.css';
 
 const TransactionList = ({ transactions }) => {
+  const { deleteDocument, response } = useFirestore('transactions');
+  console.log(response);
+
   return (
     <div>
       <ul className={styles.transactions}>
@@ -9,6 +14,7 @@ const TransactionList = ({ transactions }) => {
           <li key={transaction.id}>
             <p className={styles.name}>{transaction.name}</p>
             <p className={styles.amount}>{transaction.amount} SEK</p>
+            <button onClick={() => deleteDocument(transaction.id)}>x</button>
           </li>
         ))}
       </ul>
